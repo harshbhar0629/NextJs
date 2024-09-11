7a8aa54322a676894d11b5bf38e2e7fd8fbf4740
harshbhar0629 <harshbhar.0629@gmail.com>
2024-09-11 22:22:40 +0530

    Learning about dynamic routing

--- 02-Project/src/app/about/student/[name]/page.js
+++ 02-Project/src/app/about/student/[name]/page.js
@@ -1,12 +1,17 @@
 /** @format */
-"use client"
+"use client";
 import { usePathname } from "next/navigation";
 
-const Name = () => {
+const Name = ({ params }) => {
 	const pathname = usePathname();
 	return (
 		<div className="bg-slate-600 h-[92vh]">
-			<h1 className="text-bold text-white text-2xl text-center pt-2 ">Hey! {pathname.split("/").at(-1)}, welcome to student Dahsboard</h1>
+			<h1 className="text-bold text-white text-2xl text-center pt-2 ">
+				Hey! {pathname.split("/").at(-1)}, welcome to student Dahsboard
+			</h1>
+			<h1 className="text-bold text-white text-2xl text-center pt-2 ">
+				Name: {params.name}
+			</h1>
 		</div>
 	);
 };
--- 02-Project/src/app/about/student/page.js
+++ 02-Project/src/app/about/student/page.js
@@ -1,8 +1,10 @@
+/** @format */
+
 import Link from "next/link";
 import React from "react";
 
 const Student = () => {
-  return (
+	return (
 		<div className="h-[100vh]  mx-auto text-center my-auto bg-slate-600 text-white">
 			<h2 className="font-semibold text-2xl pt-20">This is a Student Page.</h2>
 			<br></br>
@@ -24,6 +26,6 @@ const Student = () => {
 			</Link>
 		</div>
 	);
-}
+};
 
 export default Student;
